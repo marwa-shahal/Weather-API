1d316f137420e9469574f48e78235a86f925bc5e
Marwa <marwa.al.shahal@gmail.com>
2022-12-20 11:31:51 +0200

    Weather API: Breaking the HTML in App.js into React componnets

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -2,48 +2,14 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import "./CurrentWeather.css";
 
-//import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
-
-import clear from "../img/weather-icons/clear.svg";
-import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
-import storm from "../img/weather-icons/storm.svg";
-import snow from "../img/weather-icons/snow.svg";
-import partlycloudy from "../img/weather-icons/partlycloudy.svg";
-import fog from "../img/weather-icons/fog.svg";
-import drizzle from "../img/weather-icons/drizzle.svg";
-import cloudy from "../img/weather-icons/cloudy.svg";
-import rain from "../img/weather-icons/rain.svg";
-
 export default class WeatherItem extends Component {
   render() {
-    const imageSrc = (image) => {
-      switch (image) {
-        case "clouds":
-          return cloudy;
-        case "mostlycloudy":
-          return mostlycloudy;
-        case "partlycloudy":
-          return partlycloudy;
-        case "snow":
-          return snow;
-        case "storm":
-          return storm;
-        case "rain":
-          return rain;
-        case "fog":
-          return fog;
-        case "drizzle":
-          return drizzle;
-        case "clear":
-          return clear
-      }
-    };
     return (
       <div className="current_weather">
-        <img src={imageSrc(this.props.weatherData.weather[0].main.toLowerCase())} width="300px" height="300px" />
+        <img src={this.props.imageSrc(this.props.weatherData.weather[0].main.toLowerCase())} width="300px" height="300px" />
         <h2>overcast clouds</h2>
         <h1>
-          Temperature <span>10° to 11°C</span>
+          Temperature <span>{Math.round(((this.props.weatherData.main.temp_min- 32) * 5/ 9))} to {Math.round(((this.props.weatherData.main.temp_max- 32) * 5/ 9))}°C</span>
         </h1>
         <p>
           <b>Humidity</b> <span>{this.props.weatherData.main.humidity}%</span>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,16 +3,48 @@ import WeatherItem from "./WeatherItem.js";
 import CurrentWeather from "./CurrentWeather.js";
 import "./Weather.css";
 
+import clear from "../img/weather-icons/clear.svg";
+import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "../img/weather-icons/partlycloudy.svg";
+import storm from "../img/weather-icons/storm.svg";
+import snow from "../img/weather-icons/snow.svg";
+import fog from "../img/weather-icons/fog.svg";
+import drizzle from "../img/weather-icons/drizzle.svg";
+import cloudy from "../img/weather-icons/cloudy.svg";
+import rain from "../img/weather-icons/rain.svg";
+
 export default class Weather extends Component {
 
   render() {
+    const imageSrc = (image) => {
+      switch (image) {
+        case "clouds":
+          return cloudy;
+        case "mostlycloudy":
+          return mostlycloudy;
+        case "partlycloudy":
+          return partlycloudy;
+        case "snow":
+          return snow;
+        case "storm":
+          return storm;
+        case "rain":
+          return rain;
+        case "fog":
+          return fog;
+        case "drizzle":
+          return drizzle;
+        case "clear":
+          return clear
+      }
+    };
     return (
       <>
-      <CurrentWeather weatherData = {this.props.fakeWeatherData.list[0]} />
+      <CurrentWeather weatherData = {this.props.fakeWeatherData.list[0]}  imageSrc={imageSrc}/>
       <div className="hourly_weathers">
         {this.props.fakeWeatherData.list.map((elt,index) => {
           if (index>=1 && index<8){
-          return <WeatherItem time={elt.dt_txt} svg={elt.weather[0].main.toLowerCase()} temp={elt.main.temp} />}
+          return <WeatherItem time={elt.dt_txt} svg={elt.weather[0].main.toLowerCase()} temp={elt.main.temp} imageSrc={imageSrc} />}
            }
          )}
       </div>
